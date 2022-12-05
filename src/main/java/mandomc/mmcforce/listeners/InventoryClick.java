0d16fc7297e3a06ee029c22b957eac17be1eb674
AstuteFlamez <perosinal@gmail.com>
2022-12-04 20:09:35 -0600

    v0.1

--- src/main/java/mandomc/mmcforce/listeners/InventoryClick.java
+++ src/main/java/mandomc/mmcforce/listeners/InventoryClick.java
@@ -1,9 +1,18 @@
 package mandomc.mmcforce.listeners;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 
 public class InventoryClick implements Listener {
 
+    @EventHandler
+    public void onClick(InventoryClickEvent event){
 
+        Player player = (Player) event.getWhoClicked();
+
+    }
 
 }
