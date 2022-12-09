417b583bb92b0a81c2f514c2269c4d7517c88145
AstuteFlamez <perosinal@gmail.com>
2022-12-08 21:52:40 -0600

    v0.1

--- src/main/java/mandomc/mmcforce/listeners/InventoryClick.java
+++ src/main/java/mandomc/mmcforce/listeners/InventoryClick.java
@@ -40,52 +40,52 @@ public class InventoryClick implements Listener {
         }
         if(event.getView().getTitle().equalsIgnoreCase(ChatColor.BLUE + "" + ChatColor.BOLD + "Light Side Force Powers")){
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.BLUE + "Force Heal")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.BLUE + "Force Heal", "", ChatColor.GRAY + "Regain 2 hearts!", "", ChatColor.GOLD + "Ability: Force Heal ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.BLUE + "Force Heal", 5,"", ChatColor.GRAY + "Regain 2 hearts!", "", ChatColor.GOLD + "Ability: Force Heal ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GRAY + "Force Telekinesis")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Telekinesis", "", ChatColor.GRAY + "Pushes and pulles nearby entities back!", "", ChatColor.GOLD + "Ability: Force Push ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", ChatColor.GOLD + "Ability: Force Pull ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " RIGHT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Telekinesis", 11,"", ChatColor.GRAY + "Pushes and pulles nearby entities back!", "", ChatColor.GOLD + "Ability: Force Push ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", ChatColor.GOLD + "Ability: Force Pull ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " RIGHT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GRAY + "Force Dash")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Dash", "", ChatColor.GRAY + "Gain Speed IV for 5 seconds!", "", ChatColor.GOLD + "Ability: Force Dash ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Dash", 3,"", ChatColor.GRAY + "Gain Speed IV for 5 seconds!", "", ChatColor.GOLD + "Ability: Force Dash ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GRAY + "Force Stasis")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Stasis", "", ChatColor.GRAY + "Slow nearby enemies for 10 seconds", "", ChatColor.GOLD + "Ability: Force Stasis ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Stasis", 9,"", ChatColor.GRAY + "Slow nearby enemies for 10 seconds", "", ChatColor.GOLD + "Ability: Force Stasis ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.BLUE + "Force Light")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.BLUE + "Force Light", "", ChatColor.GRAY + "Give weakness to nearby enemies!", "", ChatColor.GOLD + "Ability: Force Light ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.BLUE + "Force Light", 6,"", ChatColor.GRAY + "Give weakness to nearby enemies!", "", ChatColor.GOLD + "Ability: Force Light ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.BLUE + "Force Stealth")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.BLUE + "Force Stealth", "", ChatColor.GRAY + "Turn invisible for 15 seconds", "", ChatColor.GOLD + "Ability: Force Stealth ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.BLUE + "Force Stealth", 10,"", ChatColor.GRAY + "Turn invisible for 15 seconds", "", ChatColor.GOLD + "Ability: Force Stealth ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             event.setCancelled(true);
         }
         if(event.getView().getTitle().equalsIgnoreCase(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Dark Side Force Powers")){
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GRAY + "Force Telekinesis")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Telekinesis", "", ChatColor.GRAY + "Pushes and pulles nearby entities back!", "", ChatColor.GOLD + "Ability: Force Push ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", ChatColor.GOLD + "Ability: Force Pull ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " RIGHT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Telekinesis", 11,"", ChatColor.GRAY + "Pushes and pulles nearby entities back!", "", ChatColor.GOLD + "Ability: Force Push ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", ChatColor.GOLD + "Ability: Force Pull ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " RIGHT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GRAY + "Force Dash")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Dash", "", ChatColor.GRAY + "Gain Speed IV for 5 seconds!", "", ChatColor.GOLD + "Ability: Force Dash ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Dash", 3,"", ChatColor.GRAY + "Gain Speed IV for 5 seconds!", "", ChatColor.GOLD + "Ability: Force Dash ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_RED + "Force Lightning")){
-                player.getInventory().addItem( ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Lightning", "", ChatColor.GRAY + "Strike nearby enemies with lightning!", "", ChatColor.GOLD + "Ability: Force Lightning ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem( ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Lightning", 7,"", ChatColor.GRAY + "Strike nearby enemies with lightning!", "", ChatColor.GOLD + "Ability: Force Lightning ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_RED + "Force Choke")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Choke", "", ChatColor.GRAY + "Choke out nearby enemies!", "", ChatColor.GOLD + "Ability: Force Choke ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Choke", 1,"", ChatColor.GRAY + "Choke out nearby enemies!", "", ChatColor.GOLD + "Ability: Force Choke ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_RED + "Force Scream")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Scream", "", ChatColor.GRAY + "Gain Strength III for 5 seconds!", "", ChatColor.GOLD + "Ability: Force Scream ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Scream", 8,"", ChatColor.GRAY + "Gain Strength III for 5 seconds!", "", ChatColor.GOLD + "Ability: Force Scream ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_RED + "Force Crush")){
-               player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Crush", "", ChatColor.GRAY + "Instantly take away 2 hearts from nearby enemies!", "", ChatColor.GOLD + "Ability: Force Crush ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+               player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Crush", 2,"", ChatColor.GRAY + "Instantly take away 2 hearts from nearby enemies!", "", ChatColor.GOLD + "Ability: Force Crush ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GRAY + "Force Stasis")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Stasis", "", ChatColor.GRAY + "Slow nearby enemies for 10 seconds", "", ChatColor.GOLD + "Ability: Force Stasis ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.GRAY + "Force Stasis", 9,"", ChatColor.GRAY + "Slow nearby enemies for 10 seconds", "", ChatColor.GOLD + "Ability: Force Stasis ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_RED + "Force Drain")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Drain", "", ChatColor.GRAY + "Take 1 heart from nearby enemies for yourself!", "", ChatColor.GOLD + "Ability: Force Drain ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Drain", 4,"", ChatColor.GRAY + "Take 1 heart from nearby enemies for yourself!", "", ChatColor.GOLD + "Ability: Force Drain ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             if(event.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_RED + "Force Blind")){
-                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Blind", "", ChatColor.GRAY + "Blind everyone and everything around you!", "", ChatColor.GOLD + "Ability: Force Blind ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
+                player.getInventory().addItem(ISC.createItem(Material.FEATHER, ChatColor.DARK_RED + "Force Blind", 0,"", ChatColor.GRAY + "Blind everyone and everything around you!", "", ChatColor.GOLD + "Ability: Force Blind ->" + ChatColor.YELLOW + "" + ChatColor.BOLD + " LEFT CLICK", "", ChatColor.GRAY + "Cooldown: " + ChatColor.RED + "3 seconds"));
             }
             event.setCancelled(true);
         }
